44bf735bc46e434f29dab83b56288b3147be7ac3
@钰昭 <changliang.lcl@alibaba-inc.com>
2023-11-14 15:15:35 +0800

    fix: rm verbose code

--- aplus_mpvue/src/utils/aplus.js
+++ aplus_mpvue/src/utils/aplus.js
@@ -12,14 +12,6 @@ const aplusConfig = {
       // 注意: 目前支付宝小程序不支持该属性采集
       'props': ['data-name', 'data-pagename'],
     }],
-    'aplus-auto-exp': [{
-      'cssSelector':'.exp-txt', // 你要曝光的元素的class
-      'logkey':'autgao-exp-id', // 自定义事件的id
-      // 注意：
-      // 1、如果CSSSelector能匹配上多个元素则一定要在页面节点上埋data-tracker,否则aplusJS无法识别元素唯一性，进而导致一个列表多个元素只能触发一次曝光；
-      // 2、目前支付宝小程序不支持该属性采集
-      'props': ['data-name', 'data-pagename'],
-    }]
   }
 };
 
